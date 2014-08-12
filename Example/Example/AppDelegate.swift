64e8b9dc2b8ae6fecd16723b510d6a2fbab269b5
Heiko Dreyer <mail@boxedfolder.com>
2014-08-12 16:25:29 +0200

    Fixed dates

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Example
 //
-//  Created by Heiko Dreyer on 12.08.14.
+//  Created by Heiko Dreyer on 08/12/14.
 //  Copyright (c) 2014 boxedfolder.com. All rights reserved.
 //
 
