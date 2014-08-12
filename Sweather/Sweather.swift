29fe11b6a883d4c15542902bfee57eaa1b7458bc
Heiko Dreyer <mail@boxedfolder.com>
2014-08-12 10:47:27 +0200

    Init commit

---
+++ Sweather/Sweather.swift
@@ -0,0 +1,17 @@
+//
+//  Sweather.swift
+//  Tests
+//
+//  Created by Heiko Dreyer on 08/12/14.
+//  Copyright (c) 2014 boxedfolder.com. All rights reserved.
+//
+
+import Foundation
+
+public class Sweather {
+    public var apiKey:String
+    
+    public init(apiKey: String) {
+        self.apiKey = apiKey;
+    }
+}
