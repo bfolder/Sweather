ef76aa1f3279fb438d87d66134cccd14d7e4c67f
Heiko Dreyer <mail@boxedfolder.com>
2014-09-18 11:01:42 +0200

    Removed comment

--- Tests/Tests/SweatherTests.swift
+++ Tests/Tests/SweatherTests.swift
@@ -19,7 +19,6 @@ class SweatherTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
